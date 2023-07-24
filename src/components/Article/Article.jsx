536554a15ce513e666b76e6a4a4d1dbf5bd10b4f
Murillo_Zubk <mzubk.dev@gmail.com>
2023-07-24 19:56:26 -0300

    adicionado o article

---
+++ src/components/Article/Article.jsx
@@ -0,0 +1,30 @@
+const Article = ({ title, text, tags, image, alt }) => {
+  return (
+    <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex-col items-center gap-2">
+      <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">
+        {title}
+      </h3>
+      <div className="flex-row justify-end gap-2 pr-5 hidden sm:flex">
+        {tags.map((tag) => (
+          <span
+            key={tag}
+            className="bg-alura-100 dark:bg-dark-100 px-3 py-1 rounded-full text-gray-200 text-xs uppercase font-bold"
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
+      <div className="grid gap-1">
+        {text.map((content, index) => (
+          <p key={index} className="text-alura-200 dark:text-gray-300">
+            {content}
+          </p>
+        ))}
+      </div>
+      {image && <img className="sm:p-4" src={image}></img>}
+      {image && alt && <span className="sr-only">{alt}</span>}
+    </div>
+  );
+};
+
+export default Article;
