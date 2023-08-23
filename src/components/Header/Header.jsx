b7e1a0db1b1ab7b21e32a7feebef7d61d91be2f4
Murillo Zubk <69257259+MZubk@users.noreply.github.com>
2023-08-23 13:09:14 -0300

    alterando o nome

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({ user }) => {
     <div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
       <span className="text-gray-100">Olá, {user?.name || "Usuário"}</span>
       <h1 className="text-gray-100 text-xl sm:hover:text-2xl sm:cursor-pointer">
-        Alura Newsletter
+        Newsletter
       </h1>
       <ToggleTheme />
     </div>
